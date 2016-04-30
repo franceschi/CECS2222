96ee0f6fcc2cae234062740e7a67a34705a6ad6b
Miguel Franceschi <miguel.franceschi@msn.com>
2016-04-30 14:39:53 -0400

    c

--- WriteTextFile/StudentDate.cpp
+++ WriteTextFile/StudentDate.cpp
@@ -1,9 +1,10 @@
 #include<iostream> 
 using namespace::std; 
-#include "studentDate.h" 
+#include "StudentDate.h" 
 
 Date::Date(int tempMonth, int tempDay, int tempYear) : month(tempMonth), day(tempDay), year(tempYear) { }//end constructor 
-Date::Date(const Date &tempDate) {  
+Date::Date(const Date &tempDate) 
+{  
 	this->month=tempDate.month;  
 	this->day=tempDate.day;  
 	this->year=tempDate.year; 
@@ -27,3 +28,60 @@ void Date::setValues(int tempMonth,int tempDay,int tempYear)
 	setYear(tempYear); 
 
 }//end setValues 
+
+void Date::setMonth(int tempMonth) 
+{ 
+	this->month = checkMonth(tempMonth); 
+} //end setMonth 
+
+void Date::setDay(int tempDay) 
+{ 
+	this->day = checkDay(tempDay); 
+}//end setDay 
+
+void Date::setYear(int tempYear) 
+{ 
+	this->year = tempYear; 
+}//end setYear 
+
+int Date::getMonth() const 
+{ 
+	return this->month; 
+}//end getMonth
+int Date::getDay() const 
+{ 
+	return this->day; 
+}//end getDay 
+int Date::getYear() const 
+{
+	return this->year; 
+}//end getYear 
+
+int Date::checkMonth(int tempMonth) const
+{  
+	if(tempMonth >=1 && tempMonth <= 12) 
+	{      
+		return(tempMonth); 
+	}//end if 
+	else 
+		{       
+			cout<<"Invalid month ("<<tempMonth<<") set to 1\n";
+			return 1; 
+		}//end if 
+}//end checkMonth
+
+int Date::checkDay(int tempDay) const //utility function 
+{  
+	static const int dayPerMonth[13]={0,31,28,31,30,31,30,31,31,30,31,30,31}; 
+
+	if (tempDay > 0 && tempDay <= dayPerMonth[month]) 
+	{ 
+		return tempDay; 
+	}//end if   
+	if ( month == 2 && tempDay==29 &&  (year % 400 == 0 || (year %4 == 0 && year %100 !=0 ) ) )   
+		{    
+			return tempDay;  
+		}   
+		cout<<"Invalid Day ("<<tempDay<<") set to 1\n";   
+		return 1; 
+}//end checkDay
