afeee6adadd3714e68d601f1d2b1b8b569100f27
Miguel Franceschi <miguel.franceschi@msn.com>
2016-04-30 14:24:10 -0400

    Commit

--- WriteTextFile/StudentDate.cpp
+++ WriteTextFile/StudentDate.cpp
@@ -9,3 +9,21 @@ Date::Date(const Date &tempDate) {
 	this->year=tempDate.year; 
 
 }//end Date constructor 
+Date::~Date() 
+{ 
+	cout << "Date object destructor: " << endl;  
+	printDate(); 
+}//end 
+
+void Date::printDate() const 
+{
+	cout<<month<<"/"<<day<<"/"<<year<<endl; 
+}//end printDate 
+
+void Date::setValues(int tempMonth,int tempDay,int tempYear) 
+{  
+	setMonth(tempMonth);  
+	setDay(tempDay);  
+	setYear(tempYear); 
+
+}//end setValues 
